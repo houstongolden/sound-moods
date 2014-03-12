e78a4b9b4ad656212ae6bc61e00fcae6718f7d60
Cameron J Roe <cameronjroe@gmail.com>
2014-03-11 22:00:29 -0400

    added node app

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 
 /**
- * Module dependencies.
+ * SoundCollage
  */
 
 var express = require('express');
+var routes = require('./routes');
+var moods = require('./routes/api');
 var http = require('http');
 var path = require('path');
 
@@ -25,10 +27,15 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', function(req, res){
-	res.sendfile('views/index.html');
-});
+app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+// users
+app.get('/moods', moods.findAll);
+app.get('/moods/:id', moods.findById);
+app.post('/moods', moods.addMood);
+app.put('/moods/:id', moods.updateMood);
+app.delete('/moods/:id', moods.deleteMood);
+
+app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
